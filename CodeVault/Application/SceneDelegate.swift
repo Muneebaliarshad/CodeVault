584f0070b0de6ad750c96932513f26164dd8487d
Muneeb Arshad <ali.muneeb93@gmail.com>
2025-02-15 21:18:39 +0500

    Folders Separation

---
+++ CodeVault/Application/SceneDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  SceneDelegate.swift
+//  CodeVault
+//
+//  Created by Muneeb Ali on 15/02/2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    // MARK: - Variables
+    var window: UIWindow?
+
+    // MARK: - Delegate Methods
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+
+}
+
---
+++ CodeVault/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeVault
+//
+//  Created by Muneeb Ali on 15/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
